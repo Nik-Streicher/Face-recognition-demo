a6e456b5b8c697cf1b52717d2c95881e97a58e80
Nikolay <nikolay.sintsin@educanet.cz>
2020-12-23 00:01:08 +0100

    optimized

--- main/single_frame_recognition.py
+++ main/single_frame_recognition.py
@@ -4,14 +4,12 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 from PIL import Image, ImageDraw
 import torch
 import pickle
-import os
 import ctypes
 import cv2
 
 ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll')
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-workers = 0 if os.name == 'nt' else 4
 
 detector = MTCNN(keep_all=True)
 
@@ -38,17 +36,19 @@ for x in users:
     if (tested_image_embedding - x[1]).norm().item() < 0.8:
         print(x[0], x[2])
 
-boxes, _ = detector.detect(tested_image, )
+boxes, _ = detector.detect(tested_image)
 
 # Draw bounding box
 img_draw = tested_image.copy()
 draw = ImageDraw.Draw(img_draw)
 # draw.text(xy=(boxes[[0]], boxes[[1]]), text="Hello")
 
+my_array = []
 
 if boxes is not None:
     for box in boxes:
         draw.rectangle(box.tolist(), width=5)
+
 
 # converting PIL image to CV format
 cvImage = numpy.array(img_draw)
